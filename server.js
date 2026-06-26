d4e8170082da1780bc7850490b0a9fb409b340f2
harshit-technocrat11 <harshit2z1agrawala@gmail.com>
2026-06-26 23:07:38 +0530

    added .yaml file

--- server.js
+++ server.js
@@ -1,51 +1,47 @@
-import express from "express"
-import path from "path"
+import express from "express";
+import path from "path";
+import { MongoClient } from "mongodb";
 
+const app = express();
+const PORT = 5050;
 
-const app = express()
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public")); //express backend serves static files now
 
-const path = path();
-
+const MONGO_URL = "mongodb://admin:qwerty@localhost:27017";
+const client = new MongoClient(MONGO_URL);
 
 // get all users
 
-app.get("/getUsers", async(_req, res)=> {
-    await client.connect (URL)
+app.get("/getUsers", async (_req, res) => {
+  await client.connect(MONGO_URL);
 
-    console.log("connected succesfully")
+  console.log("connected succesfully");
 
-    const db = client.db("harshit-docker")
-    const data = await db.collection('users').find({}).toArray();
+  const db = client.db("harshit-docker-learn");
+  const data = await db.collection("users").find({}).toArray();
+  console.log(data);
 
-    client.commit();
-    client.close();
-    res.send(data)
+  client.close();
+  res.send(data);
+});
 
-})
+//POST new user
+app.post("/addUser", async (req, res) => {
+  const userObj = req.body;
+  console.log(req.body);
+  await client.connect(MONGO_URL);
+  console.log("Connected successfully to server");
 
+  const db = client.db("harshit-docker-learn");
+  const result = await db.collection("users").insertOne(userObj);
+  console.log(result);
+  console.log("data inserted in DB");
+  client.close();
 
-// post route
+  res.status(201).json({ success: true, insertedId: result.insertedId });
+});
 
-app.post("/addUser", async (_req,res)=>{
-    const UserObj = _req.body ;
-    console.log(UserObj)
-
-    await client.connect(URL)
-
-    console.log('Connected db successfully')
-
-    const db = client.db("harshit-docker")
-    const db = await db.collection('users').insertOne(userObj)
-
-
-    console.log(data)
-    console.log("data inserted in the DB")
-    client.commit()
-
-    client.close();
-
-})
-
-app.listen( PORT , ()=>{
-    console.log(`server running on port `)
-})
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT} `);
+});
